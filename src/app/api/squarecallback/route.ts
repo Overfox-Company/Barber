42acad614a8512b726a0a1ecad48d4cfd0ab85ca
joel <jzambranojazc2001gmail.com>
2024-08-13 19:35:30 -0400

    comment all funciotns of callback

--- src/app/api/squarecallback/route.ts
+++ src/app/api/squarecallback/route.ts
@@ -1,20 +1,20 @@
 // pages/api/square-callback.js
 export async function GET(req: any) {
     if (req.method === 'GET') {
-        console.log("si llega")
-        const { transactionId, status } = req?.query;
+        console.log(req)
+        //  const { transactionId, status } = req?.q;
         // Procesa la respuesta de Square aquí
         //   if (status === 'COMPLETED') {
         // Lógica para manejar transacción exitosa
-        if (transactionId) {
-            console.log(`Transaction completed successfully with ID: ${transactionId}`);
-            return new Response(JSON.stringify({
-                message: 'pago completado', transactionId
-
-            }))
-        }
+        //  if (transactionId) {
+        //    console.log(`Transaction completed successfully with ID: ${transactionId}`);
+        //    return new Response(JSON.stringify({
+        //      message: 'pago completado', transactionId
+        //
+        //    }))
+        //   }
         return new Response(JSON.stringify({
-            message: 'pago no completado'//, transactionId
+            message: 'pago  completado'//, transactionId
 
         }))
         //} else {
