44d6324278f022439e6568dd3318063c8b92fae0
joel <jzambranojazc2001gmail.com>
2024-08-22 22:48:43 -0400

    add suspense

--- src/app/squarecallback/page.tsx
+++ src/app/squarecallback/page.tsx
@@ -3,7 +3,7 @@ import { Box } from '@mui/material';
 import { NextPage } from 'next'
 import CircularProgress from '@mui/material/CircularProgress';
 import Image from 'next/image';
-import { useEffect, useState } from 'react';
+import { Suspense, useEffect, useState } from 'react';
 import FadeIn from '@/components/animation/FadeIn';
 import { NameUser } from '@/components/UI/Text';
 import ApiController from '@/controller/ApiController';
@@ -11,9 +11,28 @@ import { useRouter } from 'next/navigation';
 import { useSearchParams } from 'next/navigation';
 
 interface Props { }
-
-const Page: NextPage<Props> = ({ }) => {
+const Res = () => {
+    const searchParams = useSearchParams();
     const [state, setSatate] = useState('')
+    useEffect(() => {
+        const data = searchParams.get('data');
+
+        if (data) {
+            setSatate(data)
+            const parsedData = JSON.parse(data);
+            console.log(parsedData);
+
+            // Procesa los datos según sea necesario
+        } else {
+            console.log('No se encontraron datos en la URL.');
+        }
+    }, [searchParams]);
+    return (
+        <p>{state}</p>
+    )
+}
+const Page: NextPage<Props> = ({ }) => {
+
     const handlePayment = () => {
         const callbackUrl = encodeURIComponent('https://loacalhost:3000/api/squarescallback');
         window.location.href = `intent:#Intent;action=com.squareup.pos.action.CHARGE;S.com.squareup.pos.CLIENT_ID=sq0idp-Gg38JTyT8ySsWFFyH47jSQ;S.com.squareup.pos.WEB_CALLBACK_URI=${callbackUrl};S.com.squareup.pos.CURRENCY_CODE=USD;i.com.squareup.pos.TOTAL_AMOUNT=1;end`;
@@ -25,7 +44,7 @@ const Page: NextPage<Props> = ({ }) => {
     const [countDown, setCountDown] = useState(false)
     const [time, setTime] = useState(3)
     const route = useRouter()
-    const searchParams = useSearchParams();
+
     const handleSend = async () => {
         const json = localStorage.getItem("payment")
         const data = json ? JSON.parse(json) : null
@@ -61,19 +80,7 @@ const Page: NextPage<Props> = ({ }) => {
     useEffect(() => {
         //   handleSend()
     }, [])
-    useEffect(() => {
-        const data = searchParams.get('data');
 
-        if (data) {
-            setSatate(data)
-            const parsedData = JSON.parse(data);
-            console.log(parsedData);
-
-            // Procesa los datos según sea necesario
-        } else {
-            console.log('No se encontraron datos en la URL.');
-        }
-    }, [searchParams]);
     return <div style={{
         height: '100vh', width: '100%',
         alignItems: 'center', display: 'flex', justifyContent: 'center'
@@ -97,7 +104,11 @@ const Page: NextPage<Props> = ({ }) => {
 
                 <CircularProgress />
             </FadeIn>}
-            <p>{state}</p>
+            <Suspense fallback={<div>
+                cargando
+            </div>}>
+                <Res />
+            </Suspense>
             <br />
             {result.message ? <NameUser>{result.message}</NameUser> : <NameUser>Procesando</NameUser>}
             {result.type !== 'error' ? <NameUser size={18}>Redirecting in {time}s</NameUser> : null}
