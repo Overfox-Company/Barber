fef77954ef206be395f84364fc95ded31572a849
joel <overfoxcompany@gmail.com>
2024-10-25 16:09:21 -0400

    add

--- src/app/api/saveData/route.ts
+++ src/app/api/saveData/route.ts
@@ -3,6 +3,7 @@ import { uploadFileToCloudinary } from "@/app/functions/Cloudinary";
 import { connectDB } from "../database/MongoConnect";
 import { Personal } from "../models/Personal";
 import { Temporal } from "../models/Temporal";
+import { Payment } from "../models/Payments";
 
 
 
@@ -18,6 +19,21 @@ export async function POST(req: Request) {
         })
         await newTemporal.save()
         return new Response(JSON.stringify({ message: 'ok', data: newTemporal }))
+        /*  const pay = new Payment({
+              transaction_id: "ROrsm7FQuJmDgM6jSxqi9P3LvaB",
+              customer: "",
+              phone: "",
+              price: "28",
+              tax: '0',
+              tip: '0',
+              worker: "Frias",
+              worker_id: "66dbd8123d32dfc403b0c711",
+              total: "28",
+             method: 'cash',
+              square: true
+          })
+          await pay.save()
+          return new Response(JSON.stringify({ message: 'pago', pay }))*/
     } catch (error) {
         console.log(error)
         return new Response(JSON.stringify({ message: 'error en la api', error: error }))
